6d49264978ab27954b7bc7f9732e829b74b76550
Nissitta <159516758+Nissitta@users.noreply.github.com>
2024-02-20 18:03:29 +0530

    Update my_header.h

--- my_header.h
+++ my_header.h
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Definitions
+
 #define LONG_PRIME 4294967311l
 #define HASH_LITTLE_ENDIAN 1
 #define BUZZ_SIZE 100000
 
+//mix method for FastHash
+#define mix_fh(h)                                                                 \
+  ({                                                                           \
+    (h) ^= (h) >> 23;                                                          \
+    (h) *= 0x2127599bf4325c37ULL;                                              \
+    (h) ^= (h) >> 47;                                                          \
+  })
+
+//rot method (FastHash)
 #define rot(x,k) (((x)<<(k)) | ((x)>>(32-(k))))
 
+//mix method with 3 parameters (for HashLittle)
 #define mix(a,b,c) \
 { \
   a -= c;  a ^= rot(c, 4);  c += b; \
@@ -17,6 +29,7 @@
   c -= b;  c ^= rot(b, 4);  b += a; \
 }
 
+//definition for final method with 3 parameters (for HashLittle hf)
 #define final(a,b,c) \
 { \
   c ^= b; c -= rot(b,14); \
@@ -28,238 +41,92 @@
   c ^= b; c -= rot(b,24); \
 }
 
-char* randstring(size_t length);                    //key generator
+//Print contents of sketch
+void print_sketch(int** sketch,int depth, int width);
 
-int** create_sketch(int width, int depth);          //Initialize sketch
+//Read the data from mentioned file and store the elements in Array at ptr_Arr and return the count of elements in the Arr
+int data_from_file(char* filename,int no_words,char** ptr_Arr);
 
-int compare(char* s1, char* s2);                    //comparing 2 strings
+//32 bit key generator (can be used for setting seed also)
+uint32_t key_gen_32_bit();
 
-int** mergeSketch(int** sketch_1, int** aketch_2, int width, int depth);        //Merging 2 sketches
+//64 bit key generator
+uint64_t key_gen_64_bit();
 
+//Returns an array of 32bit keys
+uint32_t* Arr_of_Keys_32bit(int no_of_keys);
+
+//Returns an array of 64bit keys
+uint64_t* Arr_of_Keys_64bit(int no_of_keys);
+
+//Returns an array of random strings to be used as keys
+char** Arr_of_random_string(size_t length, int no_of_keys);
+
+//hmac with sha256
+int HMAC_SHA256(char* key, char *message, int width);
+
+//Fasthash for 64 bits
+uint64_t fasthash64(const void *buf, size_t len, uint64_t seed) ;
+
+//FastHash for 32 bits
+uint32_t fasthash32(const void *buf, size_t len, uint32_t seed);
+
+//Murmur Hash for 32 bits
+uint32_t MurmurHash3_x86_32 ( const void * key, int len, uint32_t seed) ;
+
+//Sub function of murmurhash
+uint32_t getblock32 ( const uint32_t * p, int i );
+
+//Sub function of murmurhash
+uint32_t rotl32 ( uint32_t x, int8_t r ) ;
+
+//Sub function of murmurhash
+uint32_t fmix32 ( uint32_t h );
+
+//FNV Hash Algorithm
+uint64_t fnv_1a(char* element, int seed);
+
+//Malloc create a new sketch
+int** create_sketch(int width, int depth);
+
+//Compute the values to be set inside sketch(64 bit hash val)
+void compute_sketch_64_bit(int** sketch, int width, int depth, char** Arr, uint64_t* Keys, int no_ele);
+
+//Compute the values to be set inside sketch(32 bit hash val)
+void compute_sketch_32_bit(int** sketch, int width, int depth, char** Arr, uint32_t* Keys, int no_ele);
+
+//Random string generator for key generation
+char* randstring(size_t length);  
+
+//Compares 2 strings
+int compare(char* s1, char* s2);     
+
+//Query any element's occurence from sketch FastHash 64 bit
+void query_element_fastHash_64_bit(int** sketch, int width, int depth,uint64_t* Keys, int no_ele, char* q);
+
+//Query any element's occurence from sketch FastHash 32 bit
+void query_element_fastHash_32_bit(int** sketch, int width, int depth,uint32_t* Keys, int no_ele, char* q);
+
+//Print the actual times of occurence of an element in a sketch
+void actual_occurence(char** Arr, char* q, int no_ele);
+
+//Merge 2 sketches
+int** mergeSketch(int** sketch_1, int** aketch_2, int width, int depth); 
+
+//Hash function for C++ implementation (Change sketch structure before using this)
 void genaj(int** hashes, int ind);
 
+//Just a not-so-special normal Hash function
 int hashstr(char* str);
 
+//Count Min Sketch Simulation
+void CMS(char** Arr1, int no_ele,int width, int depth);
+
+//Hashlittle hash function returning 32 bit hash value
 uint32_t hashlittle( const void *key, size_t length, uint32_t initval);
 
-#ifdef __GNUC__
-#define FORCE_INLINE __attribute__((always_inline)) inline
-#else
-#define FORCE_INLINE inline
-#endif
+//CMS using FastHash 64 bit
+void CMS_fastHash64(char** Arr, int no_ele, int width, int depth);
 
-// #ifndef VALGRIND
-//     switch(length)
-//     {
-//     case 12: c+=k[2]; b+=k[1]; a+=k[0]; break;
-//     case 11: c+=k[2]&0xffffff; b+=k[1]; a+=k[0]; break;
-//     case 10: c+=k[2]&0xffff; b+=k[1]; a+=k[0]; break;
-//     case 9 : c+=k[2]&0xff; b+=k[1]; a+=k[0]; break;
-//     case 8 : b+=k[1]; a+=k[0]; break;
-//     case 7 : b+=k[1]&0xffffff; a+=k[0]; break;
-//     case 6 : b+=k[1]&0xffff; a+=k[0]; break;
-//     case 5 : b+=k[1]&0xff; a+=k[0]; break;
-//     case 4 : a+=k[0]; break;
-//     case 3 : a+=k[0]&0xffffff; break;
-//     case 2 : a+=k[0]&0xffff; break;
-//     case 1 : a+=k[0]&0xff; break;
-//     case 0 : return c;              /* zero length strings require no mixing */
-//     }
-// #else /* make valgrind happy */
-//     const uint8_t  *k8;
-//     k8 = (const uint8_t *)k;
-//     switch(length)
-//     {
-//     case 12: c+=k[2]; b+=k[1]; a+=k[0]; break;
-//     case 11: c+=((uint32_t)k8[10])<<16;  /* fall through */
-//     case 10: c+=((uint32_t)k8[9])<<8;    /* fall through */
-//     case 9 : c+=k8[8];                   /* fall through */
-//     case 8 : b+=k[1]; a+=k[0]; break;
-//     case 7 : b+=((uint32_t)k8[6])<<16;   /* fall through */
-//     case 6 : b+=((uint32_t)k8[5])<<8;    /* fall through */
-//     case 5 : b+=k8[4];                   /* fall through */
-//     case 4 : a+=k[0]; break;
-//     case 3 : a+=((uint32_t)k8[2])<<16;   /* fall through */
-//     case 2 : a+=((uint32_t)k8[1])<<8;    /* fall through */
-//     case 1 : a+=k8[0]; break;
-//     case 0 : return c;
-//     }
-// #endif /* !valgrind */
-//   } else if (HASH_LITTLE_ENDIAN && ((u.i & 0x1) == 0)) {
-//     const uint16_t *k = (const uint16_t *)key;         /* read 16-bit chunks */
-//     const uint8_t  *k8;
-//     /*--------------- all but last block: aligned reads and different mixing */
-//     while (length > 12)
-//     {
-//       a += k[0] + (((uint32_t)k[1])<<16);
-//       b += k[2] + (((uint32_t)k[3])<<16);
-//       c += k[4] + (((uint32_t)k[5])<<16);
-//       mix(a,b,c);
-//       length -= 12;
-//       k += 6;
-//     }
-//     /*----------------------------- handle the last (probably partial) block */
-//     k8 = (const uint8_t *)k;
-//     switch(length)
-//     {
-//     case 12: c+=k[4]+(((uint32_t)k[5])<<16);
-//              b+=k[2]+(((uint32_t)k[3])<<16);
-//              a+=k[0]+(((uint32_t)k[1])<<16);
-//              break;
-//     case 11: c+=((uint32_t)k8[10])<<16;     /* fall through */
-//     case 10: c+=k[4];
-//              b+=k[2]+(((uint32_t)k[3])<<16);
-//              a+=k[0]+(((uint32_t)k[1])<<16);
-//              break;
-//     case 9 : c+=k8[8];                      /* fall through */
-//     case 8 : b+=k[2]+(((uint32_t)k[3])<<16);
-//              a+=k[0]+(((uint32_t)k[1])<<16);
-//              break;
-//     case 7 : b+=((uint32_t)k8[6])<<16;      /* fall through */
-//     case 6 : b+=k[2];
-//              a+=k[0]+(((uint32_t)k[1])<<16);
-//              break;
-//     case 5 : b+=k8[4];                      /* fall through */
-//     case 4 : a+=k[0]+(((uint32_t)k[1])<<16);
-//              break;
-//     case 3 : a+=((uint32_t)k8[2])<<16;      /* fall through */
-//     case 2 : a+=k[0];
-//              break;
-//     case 1 : a+=k8[0];
-//              break;
-//     case 0 : return c;                     /* zero length requires no mixing */
-//     }
-//   } else {                        /* need to read the key one byte at a time */
-//     const uint8_t *k = (const uint8_t *)key;
-//     /*--------------- all but the last block: affect some 32 bits of (a,b,c) */
-//     while (length > 12)
-//     {
-//       a += k[0];
-//       a += ((uint32_t)k[1])<<8;
-//       a += ((uint32_t)k[2])<<16;
-//       a += ((uint32_t)k[3])<<24;
-//       b += k[4];
-//       b += ((uint32_t)k[5])<<8;
-//       b += ((uint32_t)k[6])<<16;
-//       b += ((uint32_t)k[7])<<24;
-//       c += k[8];
-//       c += ((uint32_t)k[9])<<8;
-//       c += ((uint32_t)k[10])<<16;
-//       c += ((uint32_t)k[11])<<24;
-//       mix(a,b,c);
-//       length -= 12;
-//       k += 12;
-//     }
-//     /*-------------------------------- last block: affect all 32 bits of (c) */
-//     switch(length)                   /* all the case statements fall through */
-//     {
-//     case 12: c+=((uint32_t)k[11])<<24;
-//     case 11: c+=((uint32_t)k[10])<<16;
-//     case 10: c+=((uint32_t)k[9])<<8;
-//     case 9 : c+=k[8];
-//     case 8 : b+=((uint32_t)k[7])<<24;
-//     case 7 : b+=((uint32_t)k[6])<<16;
-//     case 6 : b+=((uint32_t)k[5])<<8;
-//     case 5 : b+=k[4];
-//     case 4 : a+=((uint32_t)k[3])<<24;
-//     case 3 : a+=((uint32_t)k[2])<<16;
-//     case 2 : a+=((uint32_t)k[1])<<8;
-//     case 1 : a+=k[0];
-//              break;
-//     case 0 : return c;
-//     }
-//   }
-
-
-// #define mix(h)                                                                 \
-//   ({                                                                           \
-//     (h) ^= (h) >> 23;                                                          \
-//     (h) *= 0x2127599bf4325c37ULL;                                              \
-//     (h) ^= (h) >> 47;                                                          \
-//   })
-
-
-// void MergeSketchCMS(int no_ele, int width, int depth){
-
-//     //Array 1 of strings
-//     char** Arr1 = (char**)calloc(no_ele, sizeof(char*));
-//     for(int i = 0; i < no_ele; i++){
-//         //Arr1[i] = randstring();
-//     }
-
-//     //Array 2 of strings
-//     char** Arr2 = (char**)calloc(no_ele, sizeof(char*));
-//     for(int i = 0; i < no_ele; i++){
-//         //Arr2[i] = randstring();
-//     }
-
-//     //print the array 1
-//     printf("\nArray 1\n");
-//     for(int i = 0; i < no_ele; i++){
-//         printf("%s\n", Arr1[i]);
-//     }
-
-//     //print the array 2
-//     printf("\nArray 2\n");
-//     for(int i = 0; i < no_ele; i++){
-//         printf("%s\n", Arr2[i]);
-//     }
-
-//     char** Keys = (char**)calloc(depth, sizeof(char*));
-//     for( int i = 0; i < depth; i++){
-//         Keys[i] = randstring(8);
-//         printf("%s\n",Keys[i]);
-//     }
-
-//     int** sketch_1 = create_sketch(width,depth);
-//     int** sketch_2 = create_sketch(width,depth);
-
-//     compute_sketch(sketch_1,width,depth,Arr1,Keys,no_ele);
-//     compute_sketch(sketch_2,width,depth,Arr2,Keys,no_ele);
-
-//     int** m_sketch = mergeSketch(sketch_1,sketch_2,width,depth);
-
-//     printf("\nEnter the query string(5 chars max.) : ");
-//     char* q = (char*)calloc(5,sizeof(char));
-//     scanf("%s",q);    
-    
-//     query_element(m_sketch,width,depth,Keys,no_ele,q);
-    
-//     printf("\nIn Array 1 ");
-//     actual_occurence(Arr1,q,no_ele);
-//     printf("\n");
-
-//     printf("\nIn Array 2 ");
-//     actual_occurence(Arr2,q,no_ele);
-//     printf("\n");
-
-//     for(int i = 0; i < depth; i++){
-//         free(Keys[i]);
-//     }
-//     free(Keys);
-
-//     // //Free the sketches
-//     // for(int i = 0; i<depth; i++){
-//     //     free(sketch_1[i]);
-//     // }
-//     // free(sketch_1);
-
-//     // for(int i = 0; i<depth; i++){
-//     //     free(sketch_2[i]);
-//     // }
-//     // free(sketch_2);
-
-//     // //free q_element
-//     // free(q);
-//     // //Free Array
-
-//     // for(int i = 0; i<no_ele;i++){
-//     //     free(Arr1[i]);
-//     // }
-//     // free(Arr1);
-
-//     // for(int i = 0; i<no_ele;i++){
-//     //     free(Arr2[i]);
-//     // }
-//     // free(Arr2);
-// }
+//CMS using FastHash 32 bit
+void CMS_fastHash32(char** Arr, int no_ele, int width, int depth);
